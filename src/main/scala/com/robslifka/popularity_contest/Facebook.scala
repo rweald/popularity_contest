c1420884738e00f63860aa2eb0e91f007766e409
Rob Slifka <robert.slifka@gmail.com>
2013-07-02 16:23:21 -0700

    Add Facebook querying to the mix

---
+++ src/main/scala/com/robslifka/popularity_contest/Facebook.scala
@@ -0,0 +1,18 @@
+package com.robslifka.popularity_contest
+
+import net.liftweb.json._
+import java.net.URLEncoder
+
+case class Facebook private (id:String, shares:Long, comments:Long)
+
+object Facebook {
+
+  def apply(url:String) = {
+    implicit val formats = DefaultFormats
+
+    val facebookEndpoint = "http://graph.facebook.com/?id=" + URLEncoder.encode(url, "UTF-8")
+    val json = parse(scala.io.Source.fromURL(facebookEndpoint).getLines().mkString("\n"))
+    json.extract[Facebook]
+  }
+
+}
---
+++ src/test/scala/com/robslifka/popularity_contest/FacebookSpec.scala
@@ -0,0 +1,27 @@
+package com.robslifka.popularity_contest
+
+import org.specs2.mutable.Specification
+import co.freeside.betamax.Recorder
+import co.freeside.betamax.proxy.jetty.ProxyServer
+
+class FacebookSpec extends Specification {
+
+  ".apply" should {
+    "fetch and parse JSON from the FB endpoint" in {
+      val recorder = new Recorder
+      val proxyServer = new ProxyServer(recorder)
+
+      recorder.insertTape("Facebook.apply")
+      proxyServer.start()
+
+      val fbState = Facebook("http://www.google.com")
+
+      recorder.ejectTape()
+      proxyServer.stop()
+
+      fbState.id       must_== "http://www.google.com"
+      fbState.shares   must_== 6620203
+      fbState.comments must_== 2
+    }
+  }
+}
